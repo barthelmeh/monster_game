f00d0664ea78b133930fb717c20a102e41904279
Tom Barthelmeh <tom.barthelmeh@gmail.com>
2022-03-29 12:23:09 +1300

    First commit

---
+++ test.java
@@ -0,0 +1,13 @@
+public class test {
+    public String name;
+
+    public test(String new_name) {
+        name = new_name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Your name is: " + name);
+    }
+
+
+}
