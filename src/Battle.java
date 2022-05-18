28838bf6657b19af7fd179fd878d1bea606c900a
barthelmeh <tom.barthelmeh@gmail.com>
2022-05-18 15:17:31 +1200

    Changes to battle

--- src/Battle.java
+++ src/Battle.java
@@ -1,11 +1,14 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Battle {
     private ArrayList<Monster> enemyTeam = new ArrayList<Monster>();
+    private ArrayList<Monster> playerTeam = new ArrayList<Monster>();
 
-    public Battle(ArrayList<Monster> team) {
+    public Battle(ArrayList<Monster> team, Player playerManager) {
     	enemyTeam = team;
-    	runBattle();
+        playerTeam = playerManager.getTeam();
+    	
     }
     
     public ArrayList<Monster> getTeam() {
@@ -16,8 +19,37 @@ public class Battle {
         enemyTeam = team;
     }
     
-    public void runBattle() {
-    	
+    public void runBattle(BattleScreen bsManager){
+    	do {
+            Monster enemyMonster = enemyTeam.get(0);
+            Monster playerMonster = playerTeam.get(0);
+            
+            int enemyCurrentHealth = enemyMonster.getMonsterCurrentHealth();
+            int playerCurrentHealth = playerMonster.getMonsterCurrentHealth();
+            int enemyDamage = enemyMonster.getDamage();
+            int playerDamage = playerMonster.getDamage();
+
+            // Player Attacks First
+            enemyCurrentHealth -= playerDamage;
+            enemyMonster.setCurrentHealth(enemyCurrentHealth);
+            
+            if(enemyCurrentHealth > 0) {
+                // Now Enemy Attacks
+                playerCurrentHealth -= enemyDamage;
+                playerMonster.setCurrentHealth(playerCurrentHealth);
+                if(playerCurrentHealth < 0) {
+                    playerTeam.remove(0);
+                    bsManager.updateMonsters("Player");
+                    
+                }
+            } else {
+                enemyTeam.remove(0);
+                bsManager.updateMonsters("Enemy");
+            }
+
+
+        } while(enemyTeam.size() > 0 && playerTeam.size() > 0);
+        System.out.println("finished battle");
     }
 
     
